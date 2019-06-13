e2737a2b627decb1dd41e39e5ef1198ab06673c4
toshiya <toshiya.magna0923@gmail.com>
2019-06-13 08:05:16 +0000

    kadai-repaire

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,22 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @tasks = Task.order(id: :desc).page(params[:page])
+  end
+  
+  def show
+    @tasks = Task.order(id: :desc).page(params[:page])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
   def create
       @task = current_user.tasks.build(task_params)
+      
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to root_url
@@ -15,13 +28,7 @@ class TasksController < ApplicationController
   end
 
   def edit
-    if @task.edit(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
-    end
+    @tasks = Task.order(id: :desc).page(params[:page])
   end
   
   def update
